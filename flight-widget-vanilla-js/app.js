ae650ac787173d5ce2a0592c6f641449881e76cd
MoreshwarD <moreshwar@anew-da.com>
2022-10-16 15:50:59 +0530

    added static logic to change the data dynamically

--- flight-widget-vanilla-js/app.js
+++ flight-widget-vanilla-js/app.js
@@ -24,6 +24,10 @@ let flights = [
   }
 ];
 
+const destination = ["INDIA", "DUBAI", "MONGOLIA", "NEW YORK", "FRANCE", "TOKIO"];
+const remarks = ["ON TIME", "DELAYED", "CANCELLED"];
+let hour = 15;
+
 function populateFlight() {
   for (const flight of flights){
     const tableRow = document.createElement("tr");
@@ -45,3 +49,50 @@ function populateFlight() {
 }
 
 populateFlight();
+
+function generateRandomLatter(){
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+}
+
+function generateRandomNumber(max){
+  const numbers = "0123456789";
+  if(max){
+    const newNumbers = numbers.slice(0, max);
+    return newNumbers.charAt(Math.floor(Math.random() * newNumbers.length));
+  }
+  return numbers.charAt(Math.floor(Math.random() * numbers.length));
+}
+
+function generateTime(){
+  let displayHour = hour;
+  if(hour < 12){
+    hour++;
+  }
+
+  if(hour >= 24){
+    hour = 1;
+    displayHour = hour;
+  }
+
+  if(hour < 10){
+    displayHour = "0"+hour;
+  }
+
+  return displayHour + ":" + generateRandomNumber(5) + generateRandomNumber();
+}
+
+function shuffleUp(){
+  flights.shift();
+  flights.push({
+    time: generateTime(),
+    destination: destination[Math.floor(Math.random() * destination.length)],
+    flight: generateRandomLatter() + generateRandomLatter() + " " + generateRandomNumber() + generateRandomNumber(),
+    gate: generateRandomLatter() + " " + generateRandomNumber() + generateRandomNumber(),
+    remarks: remarks[Math.floor(Math.random() * remarks.length)]
+  })
+  tableBody.textContent = "";
+  populateFlight();
+}
+
+setInterval(shuffleUp, 9000)
